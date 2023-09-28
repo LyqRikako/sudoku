ad780ca1b93aac626382d7e17e45a0eda0b6ceac
fatty419 <1297963210@qq.com>
2023-09-28 19:28:57 +0800

    style(前端样式更改): 修改前端界面的样式
    针对按钮，界面排列进行了样式修改，替换不同的颜色

--- GUI.py
+++ GUI.py
@@ -3,8 +3,90 @@ import ttkbootstrap as ttk
 from tkinter import messagebox
 from functools import partial
 import sudoku
+# 生成界面
+def generate_GUI(thread_number):
+    global notebook
+    global entries
+    global current_thread
+    global selected_block
+    root = tk.Tk()
+    root.title("数独趣多多")
+    # 创建标签页部分
+    notebook = ttk.Notebook(root)
+    notebook.pack(side='right', padx=10, pady=10)
+    for i in range(thread_number):
+        sudoku_frame = ttk.Frame(notebook)
+        notebook.add(sudoku_frame, text=f"{i + 1}")
+        entry_row = []
+        for row in range(9):
+            entry_row.append([])
+            for col in range(9):
+                entry = tk.Entry(sudoku_frame, width=2, font=("Arial", 15))
+                entry.grid(row=row, column=col)
+                entry.bind('<Button-1>', lambda e, row=row, col=col: (set_insert_color(row, col),on_cell_click(solutions,row, col)))
+                entry_row[row].append(entry)
+        entries.append(entry_row)
+    selected_block = None
+    current_thread = 0
+    # 创建主界面部分
+    main_frame = ttk.Frame(root)
+    main_frame.pack(pady=20)
+    sudoku_frame = ttk.Frame(main_frame)
+    sudoku_frame.pack(side="top")
+    # 创建难度选择和数字选择的父容器
+    choices_frame = ttk.Frame(main_frame)
+    choices_frame.pack(side="top", padx=20)
+    # 创建难度选择部分
+    difficulty_frame = ttk.Frame(choices_frame)
+    difficulty_frame.pack()
+    # 创建数字选择部分
+    number_frame = ttk.Frame(choices_frame)
+    number_frame.pack(pady=20)
+    # 创建答案按钮部分
+    solution_frame = ttk.Frame(choices_frame)
+    solution_frame.pack(side="top", padx=20, pady=20)
+    style = ttk.Style()
+    style.map("TButton",
+              foreground=[('pressed', 'white'), ('active', 'white')],
+              background=[('pressed', '!disabled', 'blue'), ('active', 'green')])
+    # 更新数独显示
+    switch_puzzle(None)
+    # 切换9道数独题目
+    notebook.bind("<<NotebookTabChanged>>", switch_puzzle)
+    # 添加难度选择按钮
+    colors = ["#C5E1A5", "#E6EE9C", "#FBC02D", "#FF6F00", "#E64A19", "#DD2C00"]
+    ttk.Label(difficulty_frame, text="难度选择",font=("",15,"bold")).grid(row=0, column=0, columnspan=3, pady=(0, 10))
+    button_difficulty = {}
+    for idx, difficulty in enumerate(["新手", "简单", "一般", "中等", "困难","专家"]):
+        button_difficulty[difficulty] = tk.Button(difficulty_frame, text=difficulty, width=8,
+                                                   command=partial(selected_difficulty, difficulty, thread_number))
+        button_difficulty[difficulty].configure(bg=colors[idx],fg="#000")
+        button_difficulty[difficulty].grid(row=1 + idx // 3, column=idx % 3, padx=5, pady=5)
+    # 添加数字选择按钮
+    ttk.Label(number_frame, text="数字选择",font=("",15,"bold")).grid(row=0, column=0, columnspan=3, pady=(0, 20))
+    for i in range(3):
+        for j in range(3):
+            button_number = i * 3 + j + 1
+            button = tk.Button(number_frame, text=str(button_number),width=8,command=partial(selected_number, button_number))
+            button.configure(bg="#01579B",fg="#FFF")
+            button.grid(row=i + 1, column=j, padx=5, pady=5)
+    # 添加显示答案按钮
+    buttonshow_answer = tk.Button(solution_frame, text="显示答案", width=12, command=partial(show_answer, root))
+    buttonshow_answer.grid(row=4, column=0, columnspan=3, padx=5, pady=5)
+    buttonshow_answer.configure(bg="#80D8FF", fg="#000")
+    # 添加提交答案按钮
+    show_completed_button = tk.Button(solution_frame, text="提交答案", width=12, command=show_completed_sudoku)
+    show_completed_button.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
+    show_completed_button.configure(bg="#80D8FF", fg="#000")
+    # 添加提示答案按钮
+    button_get_cell = tk.Button(solution_frame, text="提示答案", width=12,command=get_selected_cell)
+    button_get_cell.configure(bg="#80D8FF", fg="#000")
+    button_get_cell.grid(row=6, column=0, columnspan=3, padx=5, pady=5)
+    set_paper_color()
+    set_ques_color(current_thread)
+    root.mainloop()
 # 更新数独显示
-def update_sudoku_display(idx):
+def update_display(idx):
     for i in range(9):
         for j in range(9):
             if puzzles[idx][i][j] == 0:
@@ -14,129 +96,78 @@ def update_sudoku_display(idx):
                 entries[idx][i][j].insert(0, str(puzzles[idx][i][j]))
                 if entries[idx][i][j]['bg'] != '#fffacd':
                     entries[idx][i][j].config(bg='#fff')
-    update_gray_cells(idx)
-#切换数独题目
-def switch_sudoku(event):
+    set_ques_color(idx)
+# 切换数独题目
+def switch_puzzle(event):
     global notebook
     idx = int(notebook.index(notebook.select()))
-    update_sudoku_display(idx)
-# 函数：高亮格子并绑定数字选择事件
-def highlight_cell(row, col):
-    global current_selected_cell
+    update_display(idx)
+# 设置输入框颜色
+def set_insert_color(row, col):
+    global selected_block
     global current_thread
     current_thread = notebook.index(notebook.select())
-    # Reset previous highlight
-    if current_selected_cell:
-        entries[current_selected_cell[0]][current_selected_cell[1]][current_selected_cell[2]].config(bg='#fff')
-    # Set new highlight
+    # 重置背景白色
+    if selected_block:
+        entries[selected_block[0]][selected_block[1]][selected_block[2]].config(bg='#fff')
+    # 输入框颜色
     entries[current_thread][row][col].config(bg='#e6e6fa')
-    current_selected_cell = (current_thread, row, col)
-# 用于更新非空白格子的背景色
-def update_gray_cells(thread_idx):
+    selected_block = (current_thread, row, col)
+# 设置题目数字和背景色
+def set_ques_color(thread_idx):
     for row in range(9):
         for col in range(9):
             if puzzles[thread_idx][row][col] != 0:
                 entries[thread_idx][row][col].config(bg='#fffacd')
-def init_white_cells():
+def set_paper_color():
     global entries
     for idx in range(len(entries)):
         for row in range(9):
             for col in range(9):
                 entries[idx][row][col].config(bg='#fff')
-# 生成界面
-def generate_interface(thread_count):
-    global notebook
-    global entries
-    global current_thread
-    global current_selected_cell
-    root = tk.Tk()
-    root.title("数独趣多多")
-    # 创建标签页
-    notebook = ttk.Notebook(root)
-    notebook.pack(side='right', padx=10, pady=10)
-    for i in range(thread_count):
-        frame = ttk.Frame(notebook)
-        notebook.add(frame, text=f"{i + 1}")
-        # 为存储 Entry 对象的子列表初始化为空列表
-        entry_row = []
-        for row in range(9):
-            entry_row.append([])
-            for col in range(9):
-                entry = tk.Entry(frame, width=2, font=("Arial", 15))
-                entry.grid(row=row, column=col)
-                entry.bind('<Button-1>', lambda e, row=row, col=col: (highlight_cell(row, col),on_cell_click(solutions,row, col)))
-                entry_row[row].append(entry)
-        # 将 Entry 对象列表添加到 entries
-        entries.append(entry_row)
-    current_selected_cell = None
-    current_thread = 0
-    # 更新数独显示
-    switch_sudoku(None)
-    # 绑定事件
-    notebook.bind("<<NotebookTabChanged>>", switch_sudoku)
-    # 创建难度选择部分
-    difficulty_frame = ttk.Frame(root)
-    difficulty_frame.pack(side="top", padx=20, pady=20)
-    # 难度选择标签
-    ttk.Label(difficulty_frame, text="难度选择").grid(row=0, column=0, columnspan=3, pady=(0, 10))
-    # 难度选择按钮
-    difficulty_buttons = {}
-    for idx, difficulty in enumerate(["新手", "简单", "一般", "中等", "困难"]):
-        difficulty_buttons[difficulty] = tk.Button(difficulty_frame, text=difficulty, width=8,
-                                                   command=partial(change_difficulty, difficulty, thread_number))
-        difficulty_buttons[difficulty].grid(row=1 + idx // 3, column=idx % 3, padx=5, pady=5)
-    # 数字选择部分
-    number_frame = ttk.Frame(root)
-    number_frame.pack(side="right", padx=20, pady=20)
-    ttk.Label(number_frame, text="数字选择").grid(row=0, column=0, columnspan=3, pady=(0, 10))
-    for i in range(3):
-        for j in range(3):
-            button_number = i * 3 + j + 1
-            button = tk.Button(number_frame, text=str(button_number), width=8, command=partial(select_number, button_number))
-            button.grid(row=i + 1, column=j, padx=5, pady=5)
-    # 添加显示答案按钮
-    show_answer_button = tk.Button(number_frame, text="显示答案", width=8, command=partial(show_answer, root))
-    show_answer_button.grid(row=4, column=0, columnspan=3, padx=5, pady=5)
-    # 添加显示已完成数独的按钮
-    show_completed_button = tk.Button(number_frame, text="提交答案", width=12, command=show_completed_sudoku)
-    show_completed_button.grid(row=5, column=0, columnspan=3, padx=5, pady=5)
-    # 提示按钮
-    # button_get_cell = tk.Button(root, text="获取选中空白单元格", command=get_selected_cell())
-    # button_get_cell.pack(side="bottom", padx=20, pady=10)
-    init_white_cells()
-    update_gray_cells(current_thread)
-    root.mainloop()
+# 提示按钮功能部分
+selected_row = -1
+selected_col = -1
 def on_cell_click(solutions,row, col):
+    global selected_row, selected_col
+    selected_row = row
+    selected_col = col
     print(f"选中的空白单元格：行{row + 1}，列{col + 1}")
     print(solutions[0][0])
-# def get_selected_cell():
-#
-#     print(f"当前选中的空白单元格：行{x + 1}，列{y + 1}")
-def change_difficulty(difficulty, thread_number):
-    global current_difficulty
+def get_selected_cell():
+    global selected_row, selected_col
+    global selected_row, selected_col
+    idx = int(notebook.index(notebook.select()))
+    if selected_row >= 0 and selected_col >= 0:
+        messagebox.showinfo("提示", f"该空答案为 {solutions[idx][selected_row][selected_col]}")
+        print(f"当前选中的空白单元格：行{selected_row + 1}，列{selected_col + 1}")
+    else:
+        print("未选中任何空白单元格")
+def selected_difficulty(difficulty, thread_number):
+    global diff_chosen
     global puzzles
     global solutions
-    current_difficulty = difficulty
+    diff_chosen = difficulty
     # 生成新的数独题目
     global current_thread
     current_thread = notebook.index(notebook.select())
     puzzles , solutions = sudoku.generate_sudoku_puzzle(difficulty,thread_number)
-    # 更新数独显示
-    switch_sudoku(None)
+    # 更新数独并提示
+    switch_puzzle(None)
     messagebox.showinfo("提示", f"已切换难度为 {difficulty}")
-    # 更新非空白格子的背景色
-    init_white_cells()
-    update_gray_cells(current_thread)
-# 函数：选择数字
-def select_number(number):
+    # 更新颜色
+    set_paper_color()
+    set_ques_color(current_thread)
+# 数字选择部分
+def selected_number(number):
     global current_thread
-    global current_selected_cell
-    if current_selected_cell:
-        entries[current_selected_cell[0]][current_selected_cell[1]][current_selected_cell[2]].delete(0, tk.END)
-        entries[current_selected_cell[0]][current_selected_cell[1]][current_selected_cell[2]].insert(0, str(number))
-        current_selected_cell = None
-#显示答案
-def show_answer_grid(answer, root):
+    global selected_block
+    if selected_block:
+        entries[selected_block[0]][selected_block[1]][selected_block[2]].delete(0, tk.END)
+        entries[selected_block[0]][selected_block[1]][selected_block[2]].insert(0, str(number))
+        selected_block = None
+# 显示答案部分
+def show_answer_frame(answer, root):
     answer_window = tk.Toplevel()
     answer_window.title("答案")
     for i in range(9):
@@ -150,23 +181,24 @@ def show_answer(root):
     global current_thread
     # 获取当前选定的数独索引
     idx = int(notebook.index(notebook.select()))
-    show_answer_grid(solutions[idx], root)
+    show_answer_frame(solutions[idx], root)
+# 提示答案部分
 def show_completed_sudoku():
     # 获取当前选定的数独索引
     idx = int(notebook.index(notebook.select()))
     # 获取已完成的数独
     completed_sudoku = get_completed_sudoku(idx)
     print(completed_sudoku)
-    if sudoku.is_valid_sudoku(completed_sudoku):
+    if sudoku.check_answer(completed_sudoku):
         messagebox.showinfo("提示", "恭喜你，答案正确！")
     else:
         messagebox.showinfo("提示", "很遗憾，答案错误！")
-def get_completed_sudoku(thread_idx):
+def get_completed_sudoku(idx):
     completed_sudoku = []
     for i in range(9):
         row = []
         for j in range(9):
-            entry = entries[thread_idx][i][j]
+            entry = entries[idx][i][j]
             value = entry.get()
             if value:
                 row.append(int(value))
@@ -174,11 +206,9 @@ def get_completed_sudoku(thread_idx):
                 row.append(0)
         completed_sudoku.append(row)
     return completed_sudoku
-# 例子使用：生成9个数独题目和答案
+# 界面入口设置参数用以启动
 thread_number = 9
 level=4
 puzzles , solutions = sudoku.generate_sudoku_puzzle(level,thread_number)
-# 存储界面中的所有Entry
 entries = []
-# 生成界面
-generate_interface(thread_number)
+generate_GUI(thread_number)
